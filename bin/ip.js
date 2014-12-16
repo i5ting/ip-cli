e4ffb02c5efc3249a8ab40c0b7a442cfccc61a77
alfred sang <i5ting@126.com>
2014-12-16 20:07:41 +0800

    add copy to clipboard && bump to v1.0.3

--- bin/ip.js
+++ bin/ip.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 var ip = require('ip');
+var clip = require('cliparoo');
 
 var myip = ip.address()
+
+clip(myip, function(err){
+  if (err) throw err;
+  // console.log('copied!');
+});
+
 console.log(myip);
